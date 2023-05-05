e0e9c97db64792e46301ddcdb52fee37d7bb00d2
Andrei Zavgorodnii <andrei.z@p2p.org>
2023-05-05 23:52:34 +0300

    added ownerships checks for tge

--- on_chain_checker.sh
+++ on_chain_checker.sh
@@ -1,6 +1,144 @@
 #######################################################################################################################
 #######################################################################################################################
 #######################################################################################################################
+###########################################  CHECK BALANCES ###########################################################
+#######################################################################################################################
+#######################################################################################################################
+#######################################################################################################################
+#######################################################################################################################
+
+echo "### Checking account balances:\n"
+
+VESTING_LTI_CONTRACT_ADDRESS="neutron1a5xz4zm0gkpcf92ddm7fw8pghg2mf4wm6cyu6cgcruq35upf7auslnnfye"
+FOUNDATION_MULTISIG_ADDRESS="neutron1xumd0qd3pceu875vazn3w5m9n78cns7q5fhgj3"
+NEUTRON_DAO_ADDRESS="neutron1suhgf5svhu4usrurvxzlgn54ksxmn8gljarjtxqnapv8kjnp4nrstdxvff"
+RESERVE_CONTRACT_ADDRESS="neutron13we0myxwzlpx8l5ark8elw5gj5d59dl6cjkzmt80c5q5cv5rt54qvzkv2a"
+TOKEN_ISSUER_MULTISIG_ADDRESS="neutron18g42rexsmmnkt32nf4qxv04glp0u8w7rwvfxzr"
+LOCKDROP_CONTRACT_ADDRESS="neutron1ryhxe5fzczelcfmrhmcw9x2jsqy677fw59fsctr09srk24lt93eszwlvyj"
+AUCTION_CONTRACT_ADDRESS="neutron1qj296vdrjfhuvrm96el3yhx8rcpz4k0huqcp9vwtqzhxwrduhs8s49y3p4"
+CREDITS_CONTRACT_ADDRESS="neutron1h6828as2z5av0xqtlh4w9m75wxewapk8z9l2flvzc29zeyzhx6fqgp648z"
+AIDROP_CONTRACT_ADDRESS="neutron198sxsrjvt2v2lln2ajn82ks76k97mj72mtgl7309jehd0vy8rezs7e6c56"
+
+VESTING_LTI_CONTRACT_BALANCE_EXPECTED="68131557000000"
+FOUNDATION_MULTISIG_BALANCE_EXPECTED="11868443000000"
+NEUTRON_DAO_BALANCE_EXPECTED="300000000000000"
+RESERVE_CONTRACT_BALANCE_EXPECTED="240000000000000"
+TOKEN_ISSUER_MULTISIG_BALANCE_EXPECTED="260000000000000"
+LOCKDROP_CONTRACT_BALANCE_EXPECTED="10000000000000"
+AUCTION_CONTRACT_BALANCE_EXPECTED="40000000000000"
+CREDITS_CONTRACT_BALANCE_EXPECTED="70000000000000"
+TOTAL_SUPPLY_EXPECTED="1000000000000000"
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+VESTING_LTI_CONTRACT_BALANCE=$(neutrond q bank balances $VESTING_LTI_CONTRACT_ADDRESS --output --json | jq --raw-output ".balances[0].amount")
+
+SUM_OF_VESTING_LTI_ACCOUNTS_ALLOCATIONS=$(neutrond query wasm contract-state smart neutron1a5xz4zm0gkpcf92ddm7fw8pghg2mf4wm6cyu6cgcruq35upf7auslnnfye '{"vesting_accounts": {"limit": 100}}' --output json | jq "[.data.vesting_accounts[].info.schedules[0].end_point.amount | tonumber] | add")
+
+if [[ "$VESTING_LTI_CONTRACT_BALANCE" == "$VESTING_LTI_CONTRACT_BALANCE_EXPECTED" ]]
+  then
+       echo "VESTING_LTI_CONTRACT_BALANCE is O.K."
+  else
+       echo "[X] VESTING_LTI_CONTRACT_BALANCE is $VESTING_LTI_CONTRACT_BALANCE, expected $VESTING_LTI_CONTRACT_BALANCE_EXPECTED"
+fi
+
+if [[ "$SUM_OF_VESTING_LTI_ACCOUNTS_ALLOCATIONS" == "$VESTING_LTI_CONTRACT_BALANCE_EXPECTED" ]]
+  then
+       echo "SUM_OF_VESTING_LTI_ACCOUNTS_ALLOCATIONS is O.K."
+  else
+       echo "[X] SUM_OF_VESTING_LTI_ACCOUNTS_ALLOCATIONS is $SUM_OF_VESTING_LTI_ACCOUNTS_ALLOCATIONS, expected $VESTING_LTI_CONTRACT_BALANCE_EXPECTED"
+fi
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+FOUNDATION_MULTISIG_BALANCE=$(neutrond q bank balances $FOUNDATION_MULTISIG_ADDRESS --output --json | jq --raw-output ".balances[0].amount")
+
+if [[ "$FOUNDATION_MULTISIG_BALANCE" == "$FOUNDATION_MULTISIG_BALANCE_EXPECTED" ]]
+  then
+       echo "FOUNDATION_MULTISIG_BALANCE is O.K."
+  else
+       echo "[X] FOUNDATION_MULTISIG_BALANCE is $FOUNDATION_MULTISIG_BALANCE, expected $FOUNDATION_MULTISIG_BALANCE_EXPECTED"
+fi
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+NEUTRON_DAO_BALANCE=$(neutrond q bank balances $NEUTRON_DAO_ADDRESS --output --json | jq --raw-output ".balances[0].amount")
+
+if [[ "$NEUTRON_DAO_BALANCE" == "$NEUTRON_DAO_BALANCE_EXPECTED" ]]
+  then
+       echo "NEUTRON_DAO_BALANCE is O.K."
+  else
+       echo "[X] NEUTRON_DAO_BALANCE is $NEUTRON_DAO_BALANCE, expected $NEUTRON_DAO_BALANCE_EXPECTED"
+fi
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+RESERVE_CONTRACT_BALANCE=$(neutrond q bank balances $RESERVE_CONTRACT_ADDRESS --output --json | jq --raw-output ".balances[0].amount")
+
+if [[ "$RESERVE_CONTRACT_BALANCE" == "$RESERVE_CONTRACT_BALANCE_EXPECTED" ]]
+  then
+       echo "RESERVE_CONTRACT_BALANCE is O.K."
+  else
+       echo "[X] RESERVE_CONTRACT_BALANCE is $RESERVE_CONTRACT_BALANCE, expected $RESERVE_CONTRACT_BALANCE_EXPECTED"
+fi
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+TOKEN_ISSUER_MULTISIG_BALANCE=$(neutrond q bank balances $TOKEN_ISSUER_MULTISIG_ADDRESS --output --json | jq --raw-output ".balances[0].amount")
+
+if [[ "$TOKEN_ISSUER_MULTISIG_BALANCE" == "$TOKEN_ISSUER_MULTISIG_BALANCE_EXPECTED" ]]
+  then
+       echo "TOKEN_ISSUER_MULTISIG_BALANCE is O.K."
+  else
+       echo "[X] TOKEN_ISSUER_MULTISIG_BALANCE is $TOKEN_ISSUER_MULTISIG_BALANCE, expected $TOKEN_ISSUER_MULTISIG_BALANCE_EXPECTED"
+fi
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+LOCKDROP_CONTRACT_BALANCE=$(neutrond q bank balances $LOCKDROP_CONTRACT_ADDRESS --output --json | jq --raw-output ".balances[0].amount")
+
+if [[ "$LOCKDROP_CONTRACT_BALANCE" == "$LOCKDROP_CONTRACT_BALANCE_EXPECTED" ]]
+  then
+       echo "LOCKDROP_CONTRACT_BALANCE is O.K."
+  else
+       echo "[X] LOCKDROP_CONTRACT_BALANCE is $LOCKDROP_CONTRACT_BALANCE, expected $LOCKDROP_CONTRACT_BALANCE_EXPECTED"
+fi
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+AUCTION_CONTRACT_BALANCE=$(neutrond q bank balances $AUCTION_CONTRACT_ADDRESS --output --json | jq --raw-output ".balances[0].amount")
+
+if [[ "$AUCTION_CONTRACT_BALANCE" == "$AUCTION_CONTRACT_BALANCE_EXPECTED" ]]
+  then
+       echo "AUCTION_CONTRACT_BALANCE is O.K."
+  else
+       echo "[X] AUCTION_CONTRACT_BALANCE is $AUCTION_CONTRACT_BALANCE, expected $AUCTION_CONTRACT_BALANCE_EXPECTED"
+fi
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+CREDITS_CONTRACT_BALANCE=$(neutrond q bank balances $CREDITS_CONTRACT_ADDRESS --output --json | jq --raw-output ".balances[0].amount")
+
+if [[ "$CREDITS_CONTRACT_BALANCE" == "$CREDITS_CONTRACT_BALANCE_EXPECTED" ]]
+  then
+       echo "CREDITS_CONTRACT_BALANCE is O.K."
+  else
+       echo "[X] CREDITS_CONTRACT_BALANCE is $CREDITS_CONTRACT_BALANCE, expected $CREDITS_CONTRACT_BALANCE_EXPECTED"
+fi
+
+# ---------------------------------------------------------------------------------------------------------------------
+TOTAL_SUPPLY=$(neutrond q bank total --output json | jq ".supply[0].amount" --raw-output)
+
+if [[ "$TOTAL_SUPPLY_EXPECTED" == "$TOTAL_SUPPLY" ]]
+then
+       echo "TOTAL SUPPLY is O.K."
+  else
+       echo "[X] TOTAL SUPPLY is $TOTAL_SUPPLY, expected $TOTAL_SUPPLY_EXPECTED"
+fi
+
+#######################################################################################################################
+#######################################################################################################################
+#######################################################################################################################
 ############################################### CHECK OWNERSHIPS ######################################################
 #######################################################################################################################
 #######################################################################################################################
@@ -218,144 +356,6 @@ then
        echo "ASTROPORT_NATIVE_COIN_REGISTRY_CONTRACT_OWNER is O.K."
   else
        echo "[X] ASTROPORT_NATIVE_COIN_REGISTRY_CONTRACT_OWNER is $ASTROPORT_NATIVE_COIN_REGISTRY_CONTRACT_OWNER, expected $EXPECTED_ASTROPORT_NATIVE_COIN_REGISTRY_CONTRACT_OWNER"
-fi
-
-#######################################################################################################################
-#######################################################################################################################
-#######################################################################################################################
-###########################################  CHECK BALANCES ###########################################################
-#######################################################################################################################
-#######################################################################################################################
-#######################################################################################################################
-#######################################################################################################################
-
-echo "### Checking account balances:\n"
-
-VESTING_LTI_CONTRACT_ADDRESS="neutron1a5xz4zm0gkpcf92ddm7fw8pghg2mf4wm6cyu6cgcruq35upf7auslnnfye"
-FOUNDATION_MULTISIG_ADDRESS="neutron1xumd0qd3pceu875vazn3w5m9n78cns7q5fhgj3"
-NEUTRON_DAO_ADDRESS="neutron1suhgf5svhu4usrurvxzlgn54ksxmn8gljarjtxqnapv8kjnp4nrstdxvff"
-RESERVE_CONTRACT_ADDRESS="neutron13we0myxwzlpx8l5ark8elw5gj5d59dl6cjkzmt80c5q5cv5rt54qvzkv2a"
-TOKEN_ISSUER_MULTISIG_ADDRESS="neutron18g42rexsmmnkt32nf4qxv04glp0u8w7rwvfxzr"
-LOCKDROP_CONTRACT_ADDRESS="neutron1ryhxe5fzczelcfmrhmcw9x2jsqy677fw59fsctr09srk24lt93eszwlvyj"
-AUCTION_CONTRACT_ADDRESS="neutron1qj296vdrjfhuvrm96el3yhx8rcpz4k0huqcp9vwtqzhxwrduhs8s49y3p4"
-CREDITS_CONTRACT_ADDRESS="neutron1h6828as2z5av0xqtlh4w9m75wxewapk8z9l2flvzc29zeyzhx6fqgp648z"
-AIDROP_CONTRACT_ADDRESS="neutron198sxsrjvt2v2lln2ajn82ks76k97mj72mtgl7309jehd0vy8rezs7e6c56"
-
-VESTING_LTI_CONTRACT_BALANCE_EXPECTED="68131557000000"
-FOUNDATION_MULTISIG_BALANCE_EXPECTED="11868443000000"
-NEUTRON_DAO_BALANCE_EXPECTED="300000000000000"
-RESERVE_CONTRACT_BALANCE_EXPECTED="240000000000000"
-TOKEN_ISSUER_MULTISIG_BALANCE_EXPECTED="260000000000000"
-LOCKDROP_CONTRACT_BALANCE_EXPECTED="10000000000000"
-AUCTION_CONTRACT_BALANCE_EXPECTED="40000000000000"
-CREDITS_CONTRACT_BALANCE_EXPECTED="70000000000000"
-TOTAL_SUPPLY_EXPECTED="1000000000000000"
-
-# ---------------------------------------------------------------------------------------------------------------------
-
-VESTING_LTI_CONTRACT_BALANCE=$(neutrond q bank balances $VESTING_LTI_CONTRACT_ADDRESS --output --json | jq --raw-output ".balances[0].amount")
-
-SUM_OF_VESTING_LTI_ACCOUNTS_ALLOCATIONS=$(neutrond query wasm contract-state smart neutron1a5xz4zm0gkpcf92ddm7fw8pghg2mf4wm6cyu6cgcruq35upf7auslnnfye '{"vesting_accounts": {"limit": 100}}' --output json | jq "[.data.vesting_accounts[].info.schedules[0].end_point.amount | tonumber] | add")
-
-if [[ "$VESTING_LTI_CONTRACT_BALANCE" == "$VESTING_LTI_CONTRACT_BALANCE_EXPECTED" ]]
-  then
-       echo "VESTING_LTI_CONTRACT_BALANCE is O.K."
-  else
-       echo "[X] VESTING_LTI_CONTRACT_BALANCE is $VESTING_LTI_CONTRACT_BALANCE, expected $VESTING_LTI_CONTRACT_BALANCE_EXPECTED"
-fi
-
-if [[ "$SUM_OF_VESTING_LTI_ACCOUNTS_ALLOCATIONS" == "$VESTING_LTI_CONTRACT_BALANCE_EXPECTED" ]]
-  then
-       echo "SUM_OF_VESTING_LTI_ACCOUNTS_ALLOCATIONS is O.K."
-  else
-       echo "[X] SUM_OF_VESTING_LTI_ACCOUNTS_ALLOCATIONS is $SUM_OF_VESTING_LTI_ACCOUNTS_ALLOCATIONS, expected $VESTING_LTI_CONTRACT_BALANCE_EXPECTED"
-fi
-
-# ---------------------------------------------------------------------------------------------------------------------
-
-FOUNDATION_MULTISIG_BALANCE=$(neutrond q bank balances $FOUNDATION_MULTISIG_ADDRESS --output --json | jq --raw-output ".balances[0].amount")
-
-if [[ "$FOUNDATION_MULTISIG_BALANCE" == "$FOUNDATION_MULTISIG_BALANCE_EXPECTED" ]]
-  then
-       echo "FOUNDATION_MULTISIG_BALANCE is O.K."
-  else
-       echo "[X] FOUNDATION_MULTISIG_BALANCE is $FOUNDATION_MULTISIG_BALANCE, expected $FOUNDATION_MULTISIG_BALANCE_EXPECTED"
-fi
-
-# ---------------------------------------------------------------------------------------------------------------------
-
-NEUTRON_DAO_BALANCE=$(neutrond q bank balances $NEUTRON_DAO_ADDRESS --output --json | jq --raw-output ".balances[0].amount")
-
-if [[ "$NEUTRON_DAO_BALANCE" == "$NEUTRON_DAO_BALANCE_EXPECTED" ]]
-  then
-       echo "NEUTRON_DAO_BALANCE is O.K."
-  else
-       echo "[X] NEUTRON_DAO_BALANCE is $NEUTRON_DAO_BALANCE, expected $NEUTRON_DAO_BALANCE_EXPECTED"
-fi
-
-# ---------------------------------------------------------------------------------------------------------------------
-
-RESERVE_CONTRACT_BALANCE=$(neutrond q bank balances $RESERVE_CONTRACT_ADDRESS --output --json | jq --raw-output ".balances[0].amount")
-
-if [[ "$RESERVE_CONTRACT_BALANCE" == "$RESERVE_CONTRACT_BALANCE_EXPECTED" ]]
-  then
-       echo "RESERVE_CONTRACT_BALANCE is O.K."
-  else
-       echo "[X] RESERVE_CONTRACT_BALANCE is $RESERVE_CONTRACT_BALANCE, expected $RESERVE_CONTRACT_BALANCE_EXPECTED"
-fi
-
-# ---------------------------------------------------------------------------------------------------------------------
-
-TOKEN_ISSUER_MULTISIG_BALANCE=$(neutrond q bank balances $TOKEN_ISSUER_MULTISIG_ADDRESS --output --json | jq --raw-output ".balances[0].amount")
-
-if [[ "$TOKEN_ISSUER_MULTISIG_BALANCE" == "$TOKEN_ISSUER_MULTISIG_BALANCE_EXPECTED" ]]
-  then
-       echo "TOKEN_ISSUER_MULTISIG_BALANCE is O.K."
-  else
-       echo "[X] TOKEN_ISSUER_MULTISIG_BALANCE is $TOKEN_ISSUER_MULTISIG_BALANCE, expected $TOKEN_ISSUER_MULTISIG_BALANCE_EXPECTED"
-fi
-
-# ---------------------------------------------------------------------------------------------------------------------
-
-LOCKDROP_CONTRACT_BALANCE=$(neutrond q bank balances $LOCKDROP_CONTRACT_ADDRESS --output --json | jq --raw-output ".balances[0].amount")
-
-if [[ "$LOCKDROP_CONTRACT_BALANCE" == "$LOCKDROP_CONTRACT_BALANCE_EXPECTED" ]]
-  then
-       echo "LOCKDROP_CONTRACT_BALANCE is O.K."
-  else
-       echo "[X] LOCKDROP_CONTRACT_BALANCE is $LOCKDROP_CONTRACT_BALANCE, expected $LOCKDROP_CONTRACT_BALANCE_EXPECTED"
-fi
-
-# ---------------------------------------------------------------------------------------------------------------------
-
-AUCTION_CONTRACT_BALANCE=$(neutrond q bank balances $AUCTION_CONTRACT_ADDRESS --output --json | jq --raw-output ".balances[0].amount")
-
-if [[ "$AUCTION_CONTRACT_BALANCE" == "$AUCTION_CONTRACT_BALANCE_EXPECTED" ]]
-  then
-       echo "AUCTION_CONTRACT_BALANCE is O.K."
-  else
-       echo "[X] AUCTION_CONTRACT_BALANCE is $AUCTION_CONTRACT_BALANCE, expected $AUCTION_CONTRACT_BALANCE_EXPECTED"
-fi
-
-# ---------------------------------------------------------------------------------------------------------------------
-
-CREDITS_CONTRACT_BALANCE=$(neutrond q bank balances $CREDITS_CONTRACT_ADDRESS --output --json | jq --raw-output ".balances[0].amount")
-
-if [[ "$CREDITS_CONTRACT_BALANCE" == "$CREDITS_CONTRACT_BALANCE_EXPECTED" ]]
-  then
-       echo "CREDITS_CONTRACT_BALANCE is O.K."
-  else
-       echo "[X] CREDITS_CONTRACT_BALANCE is $CREDITS_CONTRACT_BALANCE, expected $CREDITS_CONTRACT_BALANCE_EXPECTED"
-fi
-
-# ---------------------------------------------------------------------------------------------------------------------
-TOTAL_SUPPLY=$(neutrond q bank total --output json | jq ".supply[0].amount" --raw-output)
-
-if [[ "$TOTAL_SUPPLY_EXPECTED" == "$TOTAL_SUPPLY" ]]
-then
-       echo "TOTAL SUPPLY is O.K."
-  else
-       echo "[X] TOTAL SUPPLY is $TOTAL_SUPPLY, expected $TOTAL_SUPPLY_EXPECTED"
 fi
 
 #######################################################################################################################
