2f69fdb8794cd54623b1a7fcf29f99240bda97f7
nhpd <dmitry.klpv@gmail.com>
2023-09-21 19:20:56 +0400

    adapt to local prod neutron-1 fork node

--- build_migrate_proposal_for_dao.sh
+++ build_migrate_proposal_for_dao.sh
@@ -14,7 +14,46 @@ function check_json() {
   fi
 }
 
-# TODO: current contract addresses to migrate
+BINARY=neutrond
+#TESTNET_RPC=https://rpc-falcron.pion-1.ntrn.tech:443
+TESTNET_RPC=http://65.109.239.170:26657
+DEPLOYER=neutrond_demowallet_2
+KEYS_HOME=~/.neutrond
+DEPLOYER_ADDR=$(${BINARY} keys show ${DEPLOYER} --keyring-backend test -a --home ${KEYS_HOME})
+TESTNET_CHAINID=neutron-1
+GAS_PRICES=0.9untrn
+# GAS=45000000untrn
+REST=http://65.109.239.170:1317
+
+# given the contract address on mainnet, gets contract from mainnet, stores it on testnet and returns contract code_id
+function store_code() {
+  CONTRACT_PATH=$1
+
+  RES=$($BINARY tx wasm store "$CONTRACT_PATH" \
+    --gas 5000000 \
+    --gas-prices ${GAS_PRICES} \
+    --chain-id ${TESTNET_CHAINID} \
+    --broadcast-mode=sync \
+    --from "$DEPLOYER" \
+    --keyring-backend test \
+    --output json \
+    --home ${KEYS_HOME} \
+    --node ${TESTNET_RPC} \
+    -y)
+   
+   echo $RES
+}
+
+function extract_hash() {
+    RES=$1
+    HASH=$(echo $RES | jq -r '.txhash')
+    CODE_ID=$(curl $REST/txs/$HASH | jq '.logs[0].events[1].attributes[1].value')
+    CODE_ID_NUM=${CODE_ID//\"/}
+
+    echo $CODE_ID_NUM
+}
+
+# Production contract addresses
 MAIN_DAO=neutron1suhgf5svhu4usrurvxzlgn54ksxmn8gljarjtxqnapv8kjnp4nrstdxvff
 SINGLE_PROPOSAL=neutron1436kxs0w2es6xlqpp9rd35e3d0cjnw4sv8j3a7483sgks29jqwgshlt6zh
 SINGLE_PRE_PROPOSAL=neutron1hulx7cgvpfcvg83wk5h96sedqgn72n026w6nl47uht554xhvj9nsgs8v0z
@@ -36,123 +75,48 @@ SECURITY_CW4=neutron1hyja4uyjktpeh0fxzuw2fmjudr85rk2qu98fa6nuh6d4qru9l0ssh3kgnu
 # OTHER contracts?
 
 # ===== STORE
+NEW_MAIN_DAO_CODE_RES=$(store_code "new_artifacts_dao/cwd_core.wasm")
+NEW_PROPOSAL_SINGLE_CODE_RES=$(store_code "new_artifacts_dao/cwd_proposal_single.wasm")
+NEW_PRE_PROPOSE_SINGLE_CODE_RES=$(store_code "new_artifacts_dao/cwd_pre_propose_single.wasm")
+NEW_PROPOSAL_MULTIPLE_CODE_RES=$(store_code "new_artifacts_dao/cwd_proposal_multiple.wasm")
+NEW_PRE_PROPOSE_MULTIPLE_CODE_RES=$(store_code "new_artifacts_dao/cwd_pre_propose_multiple.wasm")
+NEW_PRE_PROPOSE_OVERRULE_CODE_RES=$(store_code "new_artifacts_dao/cwd_pre_propose_overrule.wasm")
+NEW_VOTING_REGISTRY_CODE_RES=$(store_code "new_artifacts_dao/neutron_voting_registry.wasm")
+NEW_NEUTRON_VAULT_CODE_RES=$(store_code "new_artifacts_dao/neutron_vault.wasm")
+NEW_CREDITS_VAULT_CODE_RES=$(store_code "new_artifacts_dao/credits_vault.wasm")
+NEW_LOCKDROP_VAULT_CODE_RES=$(store_code "new_artifacts_dao/lockdrop_vault.wasm")
+NEW_LP_VESTING_VAULT_CODE_RES=$(store_code "new_artifacts_dao/vesting_lp_vault.wasm")
+NEW_SUBDAO_CORE_CODE_RES=$(store_code "new_artifacts_dao/cwd_subdao_core.wasm")
+NEW_SUBDAO_PRE_PROPOSE_SINGLE_CODE_RES=$(store_code "new_artifacts_dao/cwd_subdao_pre_propose_single.wasm")
+NEW_SUBDAO_TIMELOCK_SINGLE_CODE_RES=$(store_code "new_artifacts_dao/cwd_subdao_timelock_single.wasm")
+NEW_SUBDAO_PROPOSAL_SINGLE_CODE_RES=$(store_code "new_artifacts_dao/cwd_subdao_proposal_single.wasm")
+NEW_INVESTORS_VESTING_VAULT_CODE_RES=$(store_code "new_artifacts_dao/investors_vesting_vault.wasm")
+NEW_DISTRIBUTION_CODE_RES=$(store_code "new_artifacts_dao/neutron_distribution.wasm")
+NEW_RESERVE_CODE_RES=$(store_code "new_artifacts_dao/neutron_reserve.wasm")
 
-RES=$(neutrond tx wasm store new_artifacts_dao/cwd_core.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id test-1 --broadcast-mode block -y)
-NEW_MAIN_DAO_CODE_ID=$(echo $RES | jq -r '.logs[0].events[1].attributes[1].value')
-echo NEW_MAIN_DAO_CODE_ID=$NEW_MAIN_DAO_CODE_ID
-echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
-sleep 3
+echo "sleeping..."
+sleep 30
 
-RES=$(neutrond tx wasm store new_artifacts_dao/cwd_proposal_single.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id test-1 --broadcast-mode block -y)
-NEW_PROPOSAL_SINGLE_CODE_ID=$(echo $RES | jq -r '.logs[0].events[1].attributes[1].value')
-echo NEW_PROPOSAL_SINGLE_CODE_ID=$NEW_PROPOSAL_SINGLE_CODE_ID
-echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
-sleep 3
-
-RES=$(neutrond tx wasm store new_artifacts_dao/cwd_pre_propose_single.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id test-1 --broadcast-mode block -y)
-NEW_PRE_PROPOSE_SINGLE_CODE_ID=$(echo $RES | jq -r '.logs[0].events[1].attributes[1].value')
-echo NEW_PRE_PROPOSE_SINGLE_CODE_ID=$NEW_PRE_PROPOSE_SINGLE_CODE_ID
-echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
-sleep 3
-
-RES=$(neutrond tx wasm store new_artifacts_dao/cwd_proposal_multiple.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id test-1 --broadcast-mode block -y)
-NEW_PROPOSAL_MULTIPLE_CODE_ID=$(echo $RES | jq -r '.logs[0].events[1].attributes[1].value')
-echo NEW_PROPOSAL_MULTIPLE_CODE_ID=$NEW_PROPOSAL_MULTIPLE_CODE_ID
-echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
-sleep 3
-
-RES=$(neutrond tx wasm store new_artifacts_dao/cwd_pre_propose_multiple.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id test-1 --broadcast-mode block -y)
-NEW_PRE_PROPOSE_MULTIPLE_CODE_ID=$(echo $RES | jq -r '.logs[0].events[1].attributes[1].value')
-echo NEW_PRE_PROPOSE_MULTIPLE_CODE_ID=$NEW_PRE_PROPOSE_MULTIPLE_CODE_ID
-echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
-sleep 3
-
-RES=$(neutrond tx wasm store new_artifacts_dao/cwd_pre_propose_overrule.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id test-1 --broadcast-mode block -y)
-NEW_PRE_PROPOSE_OVERRULE_CODE_ID=$(echo $RES | jq -r '.logs[0].events[1].attributes[1].value')
-echo NEW_PRE_PROPOSE_OVERRULE_CODE_ID=$NEW_PRE_PROPOSE_OVERRULE_CODE_ID
-echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
-sleep 3
-
-RES=$(neutrond tx wasm store new_artifacts_dao/neutron_voting_registry.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id test-1 --broadcast-mode block -y)
-NEW_VOTING_REGISTRY_CODE_ID=$(echo $RES | jq -r '.logs[0].events[1].attributes[1].value')
-echo NEW_VOTING_REGISTRY_CODE_ID=$NEW_VOTING_REGISTRY_CODE_ID
-echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
-sleep 3
-
-RES=$(neutrond tx wasm store new_artifacts_dao/neutron_vault.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id test-1 --broadcast-mode block -y)
-NEW_NEUTRON_VAULT_CODE_ID=$(echo $RES | jq -r '.logs[0].events[1].attributes[1].value')
-echo NEW_NEUTRON_VAULT_CODE_ID=$NEW_NEUTRON_VAULT_CODE_ID
-echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
-sleep 3
-
-RES=$(neutrond tx wasm store new_artifacts_dao/credits_vault.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id test-1 --broadcast-mode block -y)
-NEW_CREDITS_VAULT_CODE_ID=$(echo $RES | jq -r '.logs[0].events[1].attributes[1].value')
-echo NEW_CREDITS_VAULT_CODE_ID=$NEW_CREDITS_VAULT_CODE_ID
-echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
-sleep 3
-
-RES=$(neutrond tx wasm store new_artifacts_dao/lockdrop_vault.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id test-1 --broadcast-mode block -y)
-NEW_LOCKDROP_VAULT_CODE_ID=$(echo $RES | jq -r '.logs[0].events[1].attributes[1].value')
-echo NEW_LOCKDROP_VAULT_CODE_ID=$NEW_LOCKDROP_VAULT_CODE_ID
-echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
-sleep 3
-
-RES=$(neutrond tx wasm store new_artifacts_dao/lp_vesting_vault.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id test-1 --broadcast-mode block -y)
-NEW_LP_VESTING_VAULT_CODE_ID=$(echo $RES | jq -r '.logs[0].events[1].attributes[1].value')
-echo NEW_LP_VESTING_VAULT_CODE_ID=$NEW_LP_VESTING_VAULT_CODE_ID
-echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
-sleep 3
-
-RES=$(neutrond tx wasm store new_artifacts_dao/cwd_subdao_core.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id test-1 --broadcast-mode block -y)
-NEW_SUBDAO_CORE_CODE_ID=$(echo $RES | jq -r '.logs[0].events[1].attributes[1].value')
-echo NEW_SUBDAO_CORE_CODE_ID=$NEW_SUBDAO_CORE_CODE_ID
-echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
-sleep 3
-
-RES=$(neutrond tx wasm store new_artifacts_dao/cwd_subdao_pre_propose_single_no_timelock.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id test-1 --broadcast-mode block -y)
-NEW_SUBDAO_PRE_PROPOSE_SINGLE_NO_TIMELOCK_CODE_ID=$(echo $RES | jq -r '.logs[0].events[1].attributes[1].value')
-echo NEW_SUBDAO_PRE_PROPOSE_SINGLE_NO_TIMELOCK_CODE_ID=$NEW_SUBDAO_PRE_PROPOSE_SINGLE_NO_TIMELOCK_CODE_ID
-echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
-sleep 3
-
-RES=$(neutrond tx wasm store new_artifacts_dao/cwd_subdao_pre_propose_single.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id test-1 --broadcast-mode block -y)
-NEW_SUBDAO_PRE_PROPOSE_SINGLE_CODE_ID=$(echo $RES | jq -r '.logs[0].events[1].attributes[1].value')
-echo NEW_SUBDAO_PRE_PROPOSE_SINGLE_CODE_ID=$NEW_SUBDAO_PRE_PROPOSE_SINGLE_CODE_ID
-echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
-sleep 3
-
-RES=$(neutrond tx wasm store new_artifacts_dao/cwd_subdao_timelock_single.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id test-1 --broadcast-mode block -y)
-NEW_SUBDAO_TIMELOCK_SINGLE_CODE_ID=$(echo $RES | jq -r '.logs[0].events[1].attributes[1].value')
-echo NEW_SUBDAO_TIMELOCK_SINGLE_CODE_ID=$NEW_SUBDAO_TIMELOCK_SINGLE_CODE_ID
-echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
-sleep 3
-
-RES=$(neutrond tx wasm store new_artifacts_dao/cwd_subdao_proposal_single.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id test-1 --broadcast-mode block -y)
-NEW_SUBDAO_PROPOSAL_SINGLE_CODE_ID=$(echo $RES | jq -r '.logs[0].events[1].attributes[1].value')
-echo NEW_SUBDAO_PROPOSAL_SINGLE_CODE_ID=$NEW_SUBDAO_PROPOSAL_SINGLE_CODE_ID
-echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
-sleep 3
-
-RES=$(neutrond tx wasm store new_artifacts_dao/investors_vesting_vault.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id test-1 --broadcast-mode block -y)
-NEW_VESTING_VAULT=$(echo $RES | jq -r '.logs[0].events[1].attributes[1].value')
-echo NEW_VESTING_VAULT=$NEW_VESTING_VAULT
-echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
-sleep 3
-
-RES=$(neutrond tx wasm store new_artifacts_dao/neutron_distribution.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id test-1 --broadcast-mode block -y)
-NEW_DISTRIBUTION_CODE_ID=$(echo $RES | jq -r '.logs[0].events[1].attributes[1].value')
-echo NEW_DISTRIBUTION_CODE_ID=$NEW_DISTRIBUTION_CODE_ID
-echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
-sleep 3
-
-RES=$(neutrond tx wasm store new_artifacts_dao/neutron_reserve.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id test-1 --broadcast-mode block -y)
-NEW_RESERVE_CODE_ID=$(echo $RES | jq -r '.logs[0].events[1].attributes[1].value')
-echo NEW_RESERVE_CODE_ID=$NEW_RESERVE_CODE_ID
-echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
-sleep 3
+NEW_MAIN_DAO_CODE_ID=$(extract_hash "$NEW_MAIN_DAO_CODE_RES")
+NEW_PROPOSAL_SINGLE_CODE_ID=$(extract_hash "$NEW_PROPOSAL_SINGLE_CODE_RES")
+NEW_PRE_PROPOSE_SINGLE_CODE_ID=$(extract_hash "$NEW_PRE_PROPOSE_SINGLE_CODE_RES")
+NEW_PROPOSAL_MULTIPLE_CODE_ID=$(extract_hash "$NEW_PROPOSAL_MULTIPLE_CODE_RES")
+NEW_PRE_PROPOSE_MULTIPLE_CODE_ID=$(extract_hash "$NEW_PRE_PROPOSE_MULTIPLE_CODE_RES")
+NEW_PRE_PROPOSE_OVERRULE_CODE_ID=$(extract_hash "$NEW_PRE_PROPOSE_OVERRULE_CODE_RES")
+NEW_VOTING_REGISTRY_CODE_ID=$(extract_hash "$NEW_VOTING_REGISTRY_CODE_RES")
+NEW_NEUTRON_VAULT_CODE_ID=$(extract_hash "$NEW_NEUTRON_VAULT_CODE_RES")
+NEW_CREDITS_VAULT_CODE_ID=$(extract_hash "$NEW_CREDITS_VAULT_CODE_RES")
+NEW_LOCKDROP_VAULT_CODE_ID=$(extract_hash "$NEW_LOCKDROP_VAULT_CODE_RES")
+NEW_LP_VESTING_VAULT_CODE_ID=$(extract_hash "$NEW_LP_VESTING_VAULT_CODE_RES")
+NEW_SUBDAO_CORE_CODE_ID=$(extract_hash "$NEW_SUBDAO_CORE_CODE_RES")
+NEW_SUBDAO_PRE_PROPOSE_SINGLE_CODE_ID=$(extract_hash "$NEW_SUBDAO_PRE_PROPOSE_SINGLE_CODE_RES")
+NEW_SUBDAO_TIMELOCK_SINGLE_CODE_ID=$(extract_hash "$NEW_SUBDAO_TIMELOCK_SINGLE_CODE_RES")
+NEW_SUBDAO_PROPOSAL_SINGLE_CODE_ID=$(extract_hash "$NEW_SUBDAO_PROPOSAL_SINGLE_CODE_RES")
+NEW_INVESTORS_VESTING_VAULT_CODE_ID=$(extract_hash "$NEW_INVESTORS_VESTING_VAULT_CODE_RES")
+NEW_DISTRIBUTION_CODE_ID=$(extract_hash "$NEW_DISTRIBUTION_CODE_RES")
+NEW_RESERVE_CODE_ID=$(extract_hash "$NEW_RESERVE_CODE_RES")
 
 # ===== MIGRATE
-
 EMPTY_MIGRATE_MSG='{}'
 MIGRATE_MSG_BASE64=$(json_to_base64 "$EMPTY_MIGRATE_MSG")
 
@@ -206,7 +170,7 @@ MIGRATE_MSGS='[
         "wasm": {
             "migrate": {
                 "contract_addr": "'"${OVERRULE_PROPOSAL}"'",
-                "new_code_id": '"${SINGLE_PROPOSAL}"',
+                "new_code_id": '"${NEW_PROPOSAL_SINGLE_CODE_ID}"',
                 "msg": "'"${MIGRATE_MSG_BASE64}"'"
             }
         }
@@ -287,12 +251,14 @@ MIGRATE_MSGS='[
         "wasm": {
             "migrate": {
                 "contract_addr": "'"${SECURITY_SINGLE_PRE_PROPOSAL}"'",
-                "new_code_id": '"${NEW_SUBDAO_PRE_PROPOSE_SINGLE_NO_TIMELOCK_CODE_ID}"',
+                "new_code_id": '"${NEW_SUBDAO_PRE_PROPOSE_SINGLE_CODE_ID}"',
                 "msg": "'"${MIGRATE_MSG_BASE64}"'"
             }
         }
-    },
+    }
 ]'
+
+# TODO: no investors vault upgrade here
 
 echo $MIGRATE_MSGS | jq . > migrate_proposal_for_dao.json
 
