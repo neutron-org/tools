32ac2396d5e0ea3a39d8d932ed3c077f731a901d
quasisamurai <quazisamurai@gmail.com>
2023-11-10 14:37:51 +0400

    upd vesting lp contract

--- store_new_contracts.sh
+++ store_new_contracts.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#RES=$(neutrond tx wasm store new_artifacts/neutron_lockdrop.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id neutron-1 --broadcast-mode block -y)
-#NEW_LOCKDROP_CODE_ID=$(echo $RES)
-#echo NEW_LOCKDROP_CODE_ID=$NEW_LOCKDROP_CODE_ID
-#echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
-#sleep 3
-#
+RES=$(neutrond tx wasm store new_artifacts/neutron_lockdrop.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id neutron-1 --broadcast-mode block -y)
+NEW_LOCKDROP_CODE_ID=$(echo $RES)
+echo NEW_LOCKDROP_CODE_ID=$NEW_LOCKDROP_CODE_ID
+echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
+sleep 3
+
 #RES=$(neutrond tx wasm store new_artifacts/lockdrop_vault.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id neutron-1 --broadcast-mode block -y)
 #NEW_LOCKDROP_VAULT_CODE_ID=$(echo $RES )
 #echo $NEW_LOCKDROP_VAULT_CODE_ID
