23d73736f22f9bd4ea99e75d7418391a7148ad13
quasisamurai <quazisamurai@gmail.com>
2023-11-13 16:26:18 +0400

    progress

--- store_new_contracts.sh
+++ store_new_contracts.sh
@@ -6,36 +6,36 @@ echo NEW_LOCKDROP_CODE_ID=$NEW_LOCKDROP_CODE_ID
 echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
 sleep 3
 
-#RES=$(neutrond tx wasm store new_artifacts/lockdrop_vault.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id neutron-1 --broadcast-mode block -y)
-#NEW_LOCKDROP_VAULT_CODE_ID=$(echo $RES )
-#echo $NEW_LOCKDROP_VAULT_CODE_ID
-#echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
-#sleep 3
+RES=$(neutrond tx wasm store new_artifacts/lockdrop_vault.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id neutron-1 --broadcast-mode block -y)
+NEW_LOCKDROP_VAULT_CODE_ID=$(echo $RES )
+echo $NEW_LOCKDROP_VAULT_CODE_ID
+echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
+sleep 3
 #
-#RES=$(neutrond tx wasm store new_artifacts/vesting_lp_vault.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id neutron-1 --broadcast-mode block -y)
-#NEW_VESTING_LP_VAULT_CODE_ID=$(echo $RES )
-#echo $NEW_VESTING_LP_VAULT_CODE_ID
-#echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
-#sleep 3
+RES=$(neutrond tx wasm store new_artifacts/vesting_lp_vault.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id neutron-1 --broadcast-mode block -y)
+NEW_VESTING_LP_VAULT_CODE_ID=$(echo $RES )
+echo $NEW_VESTING_LP_VAULT_CODE_ID
+echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
+sleep 3
 #
-#RES=$(neutrond tx wasm store new_artifacts/vesting_lp.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id test-1 --broadcast-mode block -y)
-#NEW_VESTING_LP_CODE_ID=$(echo $RES | jq -r '.logs[0].events[1].attributes[1].value')
-#echo NEW_VESTING_LP_CODE_ID=$NEW_VESTING_LP_CODE_ID
-#echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
-#sleep 3
+RES=$(neutrond tx wasm store new_artifacts/vesting_lp.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id test-1 --broadcast-mode block -y)
+NEW_VESTING_LP_CODE_ID=$(echo $RES)
+echo NEW_VESTING_LP_CODE_ID=$NEW_VESTING_LP_CODE_ID
+echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
+sleep 3
 #
-#RES=$(neutrond tx wasm store new_artifacts/neutron_reserve.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id neutron-1 --broadcast-mode block -y)
-#NEW_RESERVE_CODE_ID=$(echo $RES)
-#echo $NEW_RESERVE_CODE_ID
-#echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
-#sleep 3
+RES=$(neutrond tx wasm store new_artifacts/neutron_reserve.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id neutron-1 --broadcast-mode block -y)
+NEW_RESERVE_CODE_ID=$(echo $RES)
+echo $NEW_RESERVE_CODE_ID
+echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
+sleep 3
 #
 #
-#RES=$(neutrond tx wasm store new_artifacts/vesting_lp_vault_for_cl_pools.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id neutron-1 --broadcast-mode block -y)
-#NEW_VESTING_LP_VAULT_CL_CODE_ID=$(echo $RES)
-#echo $NEW_VESTING_LP_VAULT_CL_CODE_ID
-#echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
-#sleep 3
+RES=$(neutrond tx wasm store new_artifacts/vesting_lp_vault_for_cl_pools.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id neutron-1 --broadcast-mode block -y)
+NEW_VESTING_LP_VAULT_CL_CODE_ID=$(echo $RES)
+echo $NEW_VESTING_LP_VAULT_CL_CODE_ID
+echo "^^^^ make sure the code ID is a valid number, otherwise interrupt and investigate"
+sleep 3
 
 echo run this
 RES=$(neutrond tx wasm store new_artifacts/lockdrop_vault_for_cl_pools.wasm --from demowallet1 --gas 5000000 --gas-prices 0.0025untrn --chain-id neutron-1 --broadcast-mode block -y)
@@ -48,7 +48,7 @@ echo ""
 echo NEW_LOCKDROP_CODE_ID=454
 echo NEW_LOCKDROP_VAULT_CODE_ID=455
 echo NEW_VESTING_LP_VAULT_CODE_ID=456
-echo NEW_VESTING_LP_CODE_ID=457
-echo NEW_RESERVE_CODE_ID=458
+echo NEW_VESTING_LP_CODE_ID=461
+echo NEW_RESERVE_CODE_ID=459
 echo NEW_VESTING_LP_VAULT_CL_CODE_ID=459
 echo NEW_LOCKDROP_VAULT_CL_CODE_ID=460
